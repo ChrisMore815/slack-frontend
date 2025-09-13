94a4859f4b88be1c3b4c278f22b01a35d6499f63
ChrisMore <chrismore@gmail.com>
2025-09-13 00:05:07 -0700

    chore: init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import { Route, Routes } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+// import Icons from "./pages/Icons";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hello My Name's React
+      {/* <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/icons" element={<Icons />} />
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
