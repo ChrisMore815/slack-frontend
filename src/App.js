5120281c26a380410942d077ee68e8d686b5b60f
ChrisMore <chrismore@gmail.com>
2025-09-13 00:19:51 -0700

    chore: App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import { Route, Routes } from "react-router-dom";
 // import HomePage from "./pages/HomePage";
 // import Icons from "./pages/Icons";
+import React from "react";
 
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       Hello My Name's React
