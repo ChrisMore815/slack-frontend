dd9d60ad22c95de7d53cc25c65d7dfc36bab7478
ChrisMore <chrismore@gmail.com>
2025-09-14 08:50:08 -0700

    chore: hookapi

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -5,6 +5,7 @@ const useUsers = () => {
     const { isLoading, data } = useQuery({
         queryKey: 'user',
         queryFn: () => {
+            api.defaults.headers.common['Authorization'] = localStorage.getItem("token")
             return api.get(`${process.env.REACT_APP_BASE_URL}/user`);
         }
     });
--- src/pages/slack/Home.jsx
+++ src/pages/slack/Home.jsx
@@ -1,8 +1,11 @@
 import { Box, Text, VStack, HStack } from "@chakra-ui/react";
 import SideBar from "./sidebar/SideBar";
 import Main from './main/Main'
+import useUsers from '../../hooks/useUsers'
 
 const Home = () => {
+    const {users} = useUsers();
+    console.log(users);
     return <HStack color={"#fff"} w={"100%"} h={"100%"}>
         <SideBar />
         <Main />
